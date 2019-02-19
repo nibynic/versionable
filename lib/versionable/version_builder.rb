064aa599028f6d70a8aa27e2091d708504cd63f5
Paweł <jembezmamy@users.noreply.github.com>
2019-02-19 12:31:50 +0100

    cleanup

--- lib/versionable/version_builder.rb
+++ lib/versionable/version_builder.rb
@@ -50,31 +50,6 @@ module Versionable
       else
         []
       end
-
-
-
-      # if versioning_parent != false
-      #   versioning_parent.store_versions(author, event, params) if versioning_parent.present?
-      # else
-      #   if versions.last.present?
-      #     was = versions.last.object
-      #     was_destroyed = versions.last.event == "destroy"
-      #   end
-      #   event ||= destroyed? ? :destroy : ((was.nil? || was_destroyed) ? :create : :change)
-      #   is = serialize_for_versioning
-      #   diff = HashDiff.diff(was, is) if was.present?
-      #   if event != :change || diff.any?
-      #     version = Version.create author: author, object: is, object_changes: diff, event: event, item: self
-      #   end
-      # end
-      # if @versioning_children_were
-      #   @versioning_children_were.each do |child|
-      #     child.store_versions(author, event, params)
-      #   end
-      # end
-      # version
-
-
     end
 
     private
